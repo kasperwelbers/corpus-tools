5366bfe935b845511b69d83c0c9032154723eb03
Wouter van Atteveldt <wouter@vanatteveldt.com>
2015-02-02 13:40:51 +0100

    Move data to data folder

---
+++ R/data.r
@@ -0,0 +1,17 @@
+#' Document-term matrix based on wikinews articles on Iraq
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name wikinews_iraq
+#' @usage data(wikinews_iraq)
+#' @format iraq.dtm: A tm document-term matrix of wikinews articles, and iraq.meta: a data frame containing meta-information about the articles in the dtm
+NULL
+
+#' Tokens from Bush and Obama's State of the Union addresses
+#' 
+#' @docType data
+#' @keywords datasets
+#' @name sotu
+#' @usage data(sotu)
+#' @format sotu.dtm: A dataframe of tokens produced by using Stanford CoreNLP to lemmatize the State of the Union speeches; and sotu.meta the metadata for these documents
+NULL
